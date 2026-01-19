3cd744c0b04ece54fe5e6687a0b09e84d7810cee
Zohir Tamda <tamda.zohir.pro@gmail.com>
2026-01-20 00:25:32 +0100

    Avoid redudant code in the controller.

--- src/controllers/page_controller.cr
+++ src/controllers/page_controller.cr
@@ -7,41 +7,37 @@ class Kanjo::Controllers::PageController < ATH::Controller
   @[ARTA::Get("/")]
   # Renders the dashboard page.
   def dashboard : ATH::Response
-    journals = @journal_repository.find_all.map do |journal|
-      {
-        "date"     => journal.date.to_s("%Y-%m-%d"),
-        "content"  => journal.content,
-        "readonly" => journal.readonly?,
-        "emotion"  => journal.emotion.to_s,
-      }
-    end
-
     Kanjo.render("pages/dashboard.html.j2", {
-      :journals => journals,
+      :journals => get_all_journals,
     })
   end
 
   @[ARTA::Get("/journals/{date}")]
   # Renders the journal show page for a specific date.
   def show_journal(date : String) : ATH::Response
-    journal_obj = @journal_repository.find(date)
-    journal = {
-      "date"     => journal_obj.date.to_s("%Y-%m-%d"),
-      "content"  => journal_obj.content,
-      "readonly" => journal_obj.readonly?,
-      "emotion"  => journal_obj.emotion.to_s,
-    }
-
-    journals = @journal_repository.find_all.map do |j|
-      {
-        "date"     => j.date.to_s("%Y-%m-%d"),
-        "content"  => j.content,
-        "readonly" => j.readonly?,
-        "emotion"  => j.emotion.to_s,
-      }
-    end
+    journal = journal_to_hash(@journal_repository.find(date))
+    journals = get_all_journals
 
     Kanjo.render("pages/journal-show.html.j2", {
+      :journal  => journal,
+      :journals => journals,
+    })
+  end
+
+  @[ARTA::Get("/journals/{date}/edit")]
+  # Renders the journal edit page for a specific date.
+  def edit_journal(date : String) : ATH::Response
+    journal_obj = @journal_repository.find(date)
+
+    # Redirect to show page if journal is readonly
+    if journal_obj.readonly?
+      return ATH::RedirectResponse.new("/journals/#{date}")
+    end
+
+    journal = journal_to_hash(journal_obj)
+    journals = get_all_journals
+
+    Kanjo.render("pages/journal-edit.html.j2", {
       :journal  => journal,
       :journals => journals,
     })
@@ -50,14 +46,7 @@ class Kanjo::Controllers::PageController < ATH::Controller
   @[ARTA::Get("/journals")]
   # Renders the journals page.
   def journals : ATH::Response
-    journals = @journal_repository.find_all.map do |journal|
-      {
-        "date"     => journal.date.to_s("%Y-%m-%d"),
-        "content"  => journal.content,
-        "readonly" => journal.readonly?,
-        "emotion"  => journal.emotion.to_s,
-      }
-    end
+    journals = get_all_journals
 
     Kanjo.render("pages/journals.html.j2", {
       :journals => journals,
@@ -67,17 +56,26 @@ class Kanjo::Controllers::PageController < ATH::Controller
   @[ARTA::Get("/about")]
   # Renders the about page.
   def about : ATH::Response
-    journals = @journal_repository.find_all.map do |journal|
-      {
-        "date"     => journal.date.to_s("%Y-%m-%d"),
-        "content"  => journal.content,
-        "readonly" => journal.readonly?,
-        "emotion"  => journal.emotion.to_s,
-      }
-    end
+    journals = get_all_journals
 
     Kanjo.render("pages/about.html.j2", {
       :journals => journals,
     })
+  end
+
+  # Private methods to avoid code duplication
+  private def journal_to_hash(journal) : Hash(String, String | Bool)
+    {
+      "date"     => journal.date.to_s("%Y-%m-%d"),
+      "content"  => journal.content,
+      "readonly" => journal.readonly?,
+      "emotion"  => journal.emotion.to_s,
+    }
+  end
+
+  private def get_all_journals : Array(Hash(String, String | Bool))
+    @journal_repository.find_all.map do |journal|
+      journal_to_hash(journal)
+    end
   end
 end
